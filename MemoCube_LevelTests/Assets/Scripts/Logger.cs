c3bbe817ed982ccdfa62160d2318612c0a8a0899
María Simó Alonso <malonso@bigpoint.net>
2016-11-04 20:50:49 +0100

    PROTOTYPE: Prototype to investigate switch between levels and camera logs

---
+++ MemoCube_LevelTests/Assets/Scripts/Logger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Logger : MonoBehaviour {
+
+    private Camera cameraOrtho;
+    public Text cameraLoggerText;
+    public Text counterText;
+
+	// Use this for initialization
+	void Start () {
+
+        //cameraLoggerText = GetComponent<Text>();
+        cameraOrtho = GetComponent<Camera>();
+        int nCubes = (GameObject.FindGameObjectsWithTag("cube").Length);
+        counterText.text = "Num Cubes: " + nCubes.ToString() + "\n" +
+             "Num Pairs: " + (nCubes / 2).ToString() + "\n" +
+             "Leftovers: " + (nCubes % 2).ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+ 
+        cameraLoggerText.text = "CSize: " + cameraOrtho.orthographicSize + "\n" +
+            "CPosition: " + cameraOrtho.transform.localPosition + "\n" +
+            "CRotation: " + cameraOrtho.transform.localRotation+ "\n";
+
+        //Transform parentTransform = cameraOrtho.GetComponentInParent<GameObject>().transform;
+        Transform parentTransform = (GameObject.FindGameObjectWithTag("targetCamera").transform);
+
+        cameraLoggerText.text += 
+            "CParentPos: " + parentTransform.transform.localPosition + "\n" +
+            "CParentRot: " + parentTransform.transform.localRotation + "\n";
+
+    }
+}
---
+++ MemoCube_LevelTests/Assets/Scripts/Pager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Pager : MonoBehaviour {
+
+    private int currentSceneIndex;
+    private int maxScenes;
+
+    // Use this for initialization
+    void Start () {
+
+        //currentSceneIndex = int.Parse(sceneName.Substring("Scn_".Length,sceneName.Length));
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        maxScenes = SceneManager.sceneCountInBuildSettings;
+    }
+	
+
+    public void onReleaseNext()
+    {
+        if(currentSceneIndex>= maxScenes-1)
+        {
+            currentSceneIndex = 0;
+
+        }else
+        {
+            currentSceneIndex++;
+        }
+       
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    public void onReleaseBack()
+    {
+        if (currentSceneIndex <= 0 )
+        {
+            currentSceneIndex = maxScenes-1;
+
+        }
+        else
+        {
+            currentSceneIndex--;
+        }
+       
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
